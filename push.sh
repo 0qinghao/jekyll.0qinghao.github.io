74cfad49de009c4542d969e37fcf717d9861c1ff
0qinghao <shu_rin@shu.edu.cn>
2020-07-04 19:28:21 +0800

    构建方案敲定：github上jekyll.0qinghao.github.io项目作为构建源码，构建结果存放到docs；github上0qinghao.github.io以及coding上0qinghao作为不托管编译的静态网页，只推送docs文件夹内的内容

--- push.sh
+++ push.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 bundle exec jekyll build --destination ./docs
 cp ./CNAME ./docs/
-cp ./.nojekyll ./docs/
 
 # touch docs/baidu_sitemap.xml
 # sed -e 's/0qinghao.github.io\/inforest/7u7d9c.coding-pages.com/' docs/sitemap.xml> docs/baidu_sitemap.xml
@@ -9,5 +8,6 @@ cp ./.nojekyll ./docs/
 git add -A;git commit -m "$*";git push origin master;
 
 cd docs
+touch .nojekyll
 git add -A;git commit -m "$*";git push coding master;
 git add -A;git commit -m "$*";git push githubpage master;
