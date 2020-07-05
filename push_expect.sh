c2984bda0cc05a2f21941daf2805bae480eb4ab6
0qinghao <shu_rin@shu.edu.cn>
2020-07-05 11:57:29 +0800

    修改about 博客链接。开启CDN。修改推送脚本。

--- push_expect.sh
+++ push_expect.sh
@@ -4,13 +4,13 @@ set codingpw [lindex $argv 1]
 
 spawn git push origin master
 expect {
-    "Password" {send "$githubpw"}
+    "Password*" {send "$githubpw\r\n"}
 }
 
 cd docs
 spawn git push coding master
-expect "Password"
-send "$codingpw"
+expect "Password*"
+send "$codingpw\r\n"
 spawn git push githubpage master
-expect "Password"
-send "$githubpw"
+expect "Password*"
+send "$githubpw\r\n"
