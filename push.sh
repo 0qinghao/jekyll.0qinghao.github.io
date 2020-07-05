494381457ca4806f83a5851187dc38331f4e75bf
0qinghao <shu_rin@shu.edu.cn>
2020-07-05 14:50:20 +0800

    修改移动设备副标题css 防止换行 +调整行距

--- push.sh
+++ push.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+bundle exec jekyll build --destination ./docs
+cp ./CNAME ./docs/
+touch ./docs/.nojekyll
+
 read -s -p 'github password:' githubpw
 echo
 read -s -p 'coding password:' codingpw
 echo
 
-bundle exec jekyll build --destination ./docs
-cp ./CNAME ./docs/
-touch ./docs/.nojekyll
 git add -A;git commit -m "$*";
 cd docs
 git add -A;git commit -m "$*";
