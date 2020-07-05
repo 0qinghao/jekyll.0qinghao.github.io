0941b4d25fccb14ec77ff00bd680ff33bb764b06
0qinghao <shu_rin@shu.edu.cn>
2020-07-05 11:43:34 +0800

    修改about 博客链接。开启CDN。修改推送脚本。

--- push.sh
+++ push.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 read -s -p 'github password:' githubpw
+echo
 read -s -p 'coding password:' codingpw
+echo
 
 bundle exec jekyll build --destination ./docs
 cp ./CNAME ./docs/
 touch ./docs/.nojekyll
-
+git add -A;git commit -m "$*";
+cd docs
+git add -A;git commit -m "$*";
+cd ..
 # touch docs/baidu_sitemap.xml
 # sed -e 's/0qinghao.github.io\/inforest/7u7d9c.coding-pages.com/' docs/sitemap.xml> docs/baidu_sitemap.xml
-
-git add -A;git commit -m "$*";
+/usr/bin/expect <<-EOF
 spawn git push origin master;
 expect "Password"
 send "$githubpw"
 
 cd docs
-git add -A;git commit -m "$*";
 spawn git push coding master;
 expect "Password"
 send "$codingpw"
 spawn git push githubpage master;
 expect "Password"
 send "$githubpw"
+expect eof
+exit 0
+EOF
