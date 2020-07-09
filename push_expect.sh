c8dbf6840c3c38415c86ca59f219774da0d0ee1a
0qinghao <shu_rin@shu.edu.cn>
2020-07-09 20:47:01 +0800

    test push docs first

--- push_expect.sh
+++ push_expect.sh
@@ -2,14 +2,7 @@
 set githubpw [lindex $argv 0]
 set codingpw [lindex $argv 1]
 
-spawn git push origin master
-expect {
-    "Password*" {send "$githubpw\r\n"}
-}
-expect eof
-
 cd docs
-
 spawn git push coding master
 expect {
     "Password*" {send "$codingpw\r\n"}
@@ -17,6 +10,13 @@ expect {
 expect eof
 
 spawn git push githubpage master
+expect {
+    "Password*" {send "$githubpw\r\n"}
+}
+expect eof
+
+cd ..
+spawn git push origin master
 expect {
     "Password*" {send "$githubpw\r\n"}
 }
